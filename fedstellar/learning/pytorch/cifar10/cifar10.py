aab77343d0488b9a05b2b3ac5d5f5e85b5f76513
janosch <janosch.baltensperger>
2023-06-22 14:09:39 +0200

    update cifar10 dataset to match fashion & mnist.

--- fedstellar/learning/pytorch/cifar10/cifar10.py
+++ fedstellar/learning/pytorch/cifar10/cifar10.py
@@ -2,18 +2,44 @@
 # This file is part of the fedstellar framework (see https://github.com/enriquetomasmb/fedstellar).
 # Copyright (c) 2023 Enrique Tomás Martínez Beltrán.
 #
-from math import floor
+import os
+import sys
 
-import lightning as pl
 # To Avoid Crashes with a lot of nodes
 import torch.multiprocessing
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms as T
 from torchvision.datasets import CIFAR10
 
+torch.multiprocessing.set_sharing_strategy("file_system")
+
 
 class CIFAR10Dataset(Dataset):
-    def __init__(self, normalization="cifar10", loading="torchvision", sub_id=0, number_sub=1, num_workers=4, batch_size=32, iid=True, root_dir="./data"):
+    """
+    LightningDataModule of partitioned MNIST.
+
+    Args:
+        sub_id: Subset id of partition. (0 <= sub_id < number_sub)
+        number_sub: Number of subsets.
+        batch_size: The batch size of the data.
+        num_workers: The number of workers of the data.
+        val_percent: The percentage of the validation set.
+    """
+
+    # Singleton
+    cifar10_train = None
+    cifar10_val = None
+
+    def __init__(
+            self,
+            normalization="cifar10",
+            loading="torchvision",
+            sub_id=0,
+            number_sub=1,
+            num_workers=2,
+            batch_size=32,
+            iid=True,
+    ):
         super().__init__()
         self.train_set = None
         self.test_set = None
@@ -22,33 +48,54 @@ class CIFAR10Dataset(Dataset):
         self.num_workers = num_workers
         self.batch_size = batch_size
         self.iid = iid
-        self.root_dir = root_dir
         self.loading = loading
         self.normalization = normalization
         self.mean = self.set_normalization(normalization)["mean"]
         self.std = self.set_normalization(normalization)["std"]
 
-        self.train_set = self.get_dataset(
-            train=True,
-            transform=T.Compose(
-                [
-                    T.RandomCrop(32, padding=4),
-                    T.RandomHorizontalFlip(),
-                    T.ToTensor(),
-                    T.Normalize(self.mean, self.std),
-                ]
-            ),
-        )
+        # Singletons of MNIST train and test datasets
+        if not os.path.exists(f"{sys.path[0]}/data"):
+            os.makedirs(f"{sys.path[0]}/data")
 
-        self.test_set = self.get_dataset(
-            train=False,
-            transform=T.Compose(
-                [
-                    T.ToTensor(),
-                    T.Normalize(self.mean, self.std),
+        if CIFAR10Dataset.cifar10_train is None:
+            CIFAR10Dataset.cifar10_train = CIFAR10(
+                f"{sys.path[0]}/data", train=True, download=True, transform=T.Compose(
+                    [
+                        T.RandomCrop(32, padding=4),
+                        T.RandomHorizontalFlip(),
+                        T.ToTensor(),
+                        T.Normalize(self.mean, self.std),
+                    ])
+            )
+            if not iid:
+                sorted_indexes = CIFAR10Dataset.cifar10_train.targets.sort()[1]
+                CIFAR10Dataset.cifar10_train.targets = (
+                    CIFAR10Dataset.cifar10_train.targets[sorted_indexes]
+                )
+                CIFAR10Dataset.cifar10_train.data = CIFAR10Dataset.cifar10_train.data[
+                    sorted_indexes
                 ]
-            ),
-        )
+        if CIFAR10Dataset.cifar10_val is None:
+            CIFAR10Dataset.cifar10_val = CIFAR10(
+                f"{sys.path[0]}/data", train=False, download=True, transform=T.Compose(
+                    [
+                        T.ToTensor(),
+                        T.Normalize(self.mean, self.std),
+                    ])
+            )
+            if not iid:
+                sorted_indexes = CIFAR10Dataset.cifar10_val.targets.sort()[1]
+                CIFAR10Dataset.cifar10_val.targets = CIFAR10Dataset.cifar10_val.targets[
+                    sorted_indexes
+                ]
+                CIFAR10Dataset.cifar10_val.data = CIFAR10Dataset.cifar10_val.data[
+                    sorted_indexes
+                ]
+        if self.sub_id + 1 > self.number_sub:
+            raise ("Not exist the subset {}".format(self.sub_id))
+
+        self.train_set = CIFAR10Dataset.cifar10_train
+        self.test_set = CIFAR10Dataset.cifar10_val
 
         if not self.iid:
             # if non-iid, sort the dataset
@@ -62,7 +109,8 @@ class CIFAR10Dataset(Dataset):
         return dataset
 
     def set_normalization(self, normalization):
-        # Image classification on the CIFAR10 dataset - Albumentations Documentation https://albumentations.ai/docs/autoalbument/examples/cifar10/
+        # Image classification on the CIFAR10 dataset
+        # Albumentations Documentation https://albumentations.ai/docs/autoalbument/examples/cifar10/
         if normalization == "cifar10":
             mean = (0.4914, 0.4822, 0.4465)
             std = (0.2471, 0.2435, 0.2616)
@@ -73,17 +121,3 @@ class CIFAR10Dataset(Dataset):
         else:
             raise NotImplementedError
         return {"mean": mean, "std": std}
-
-    def get_dataset(self, train, transform, download=True):
-        if self.loading == "torchvision":
-            dataset = CIFAR10(
-                root=self.root_dir,
-                train=train,
-                transform=transform,
-                download=download,
-            )
-        elif self.loading == "custom":
-            raise NotImplementedError
-        else:
-            raise NotImplementedError
-        return dataset
