dd23341dfa95fd925eef9700f3a2ab557b1fd969
janousy <janosch.baltensperger@gmail.com>
2023-07-24 17:53:28 +0200

    DONE evaluation.

--- evaluation/baseline/baseline_overview_f1.py
+++ evaluation/baseline/baseline_overview_f1.py
@@ -42,7 +42,7 @@ for i, dataset in enumerate(datasets):
         ds_agg = dataset.filter(regex=(f'{aggregator}_'))
         ds_max_agg_name = ds_agg.filter(regex='__MAX').columns[0]
         ds_min_agg_name = ds_agg.filter(regex='__MIN').columns[0]
-        dataset[f'{aggregator}_ERROR'] = ds_agg[ds_max_agg_name] - ds_agg[ds_min_agg_name]
+        dataset[f'{aggregator}_ERROR'] = (ds_agg[ds_max_agg_name] - ds_agg[ds_min_agg_name])/2
 
     ds_noerr = dataset[dataset.columns.drop(list(dataset.filter(regex='__MAX')))]
     ds_noerr = ds_noerr[ds_noerr.columns.drop(list(ds_noerr.filter(regex='__MIN')))]
@@ -64,12 +64,12 @@ overview_f1 = overview_f1_mean.copy()
 for c in overview_f1.columns:
     overview_f1[c] = overview_f1[c].apply(lambda x: "{:.3f}".format(x) + "\u00B1")
     overview_f1[c] = overview_f1[c] + overview_f1_error[c].apply(lambda x: "{:.3f}".format(x))
-print(overview_f1.head())
+print(overview_f1)
 
 overview = overview_f1.sort_index() # alphabetically
 styles = css_helper.get_table_styles()
 overview_f1_style = overview_f1.style.set_table_styles(styles)
 overview_f1_style = overview_f1_style.set_caption(f'F1-Score')
 file = "figures/baseline_overview_f1.png"
-dfi.export(overview_f1_style, file, dpi=300, fontsize=12)
+dfi.export(overview_f1_style, file, dpi=600, fontsize=12)
 print("Saved tabel to: " + file)
